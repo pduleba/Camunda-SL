05d3ce04cb50f57dc8fce66c32e9d3fa96a7febf
pduleba <pduleba@pgs-soft.com>
2019-02-22 14:36:38 +0100

    Step 3 :: Update and revert cnahges in *.bpmn file

--- developing-process-applications/src/main/java/com/pgs/camunda/poc/LoanApprovalApplication.java
+++
@@ -1,9 +0,0 @@
-package com.pgs.camunda.poc;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Load Approval App")
-public class LoanApprovalApplication extends ServletProcessApplication {
-    // camunda
-}
