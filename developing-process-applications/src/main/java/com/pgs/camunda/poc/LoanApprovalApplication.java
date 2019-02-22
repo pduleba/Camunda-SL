39140f82e1cfa3aa841c093402b18a64561b0c67
pduleba <pduleba@pgs-soft.com>
2019-02-22 10:45:54 +0100

    Step 2: add a process application class and the deployment descriptor

---
+++ developing-process-applications/src/main/java/com/pgs/camunda/poc/LoanApprovalApplication.java
@@ -0,0 +1,8 @@
+package com.pgs.camunda.poc;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Load Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication {
+}
